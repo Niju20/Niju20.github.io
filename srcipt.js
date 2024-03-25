0a0c19aba81eaf2ca4ac4e053ba20a42d737ecda
Niju20 <91789741+Niju20@users.noreply.github.com>
2024-03-25 17:31:57 +0800

    Update srcipt.js
    Signed-off-by: Niju20 <91789741+Niju20@users.noreply.github.com>

--- srcipt.js
+++ srcipt.js
@@ -17,7 +17,28 @@ function checkCookie_() {
   }
 }
 
+function doOnce() {
+  if (
+    !document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("doSomethingOnlyOnce"))
+  ) {
+    // Note that we are setting `SameSite=None;` in this example because the example
+    // needs to work cross-origin.
+    // It is more common not to set the `SameSite` attribute, which results in the default,
+    // and more secure, value of `SameSite=Lax;`
+    document.cookie =
+      "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
 
+    const output = document.getElementById("do-once");
+alert("ようこそw");  
+}
+
+function clearOutputDoOnce() {
+  const output = document.getElementById("do-once");
+  output.textContent = "";
+}
+}
 
 
 
